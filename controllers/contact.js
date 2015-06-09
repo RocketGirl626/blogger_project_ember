92188f79e60395d4d7bd2fe57d36036e2670effe
Mom <mom@Michaels-MacBook-Pro-2.local>
2015-06-09 15:22:24 -0700

    beginning modules

--- controllers/contact.js
+++ controllers/contact.js
@@ -2,6 +2,8 @@ Blogger.ContactController = Ember.Controller.extend({
   actions: {
     sendMessage: function() {
       var message = prompt('Type your message here:');
+      this.set('confirmationNumber', Math.round(Math.random() * 100000));
+      this.set('messageSent', true);
     }
   }
 });
